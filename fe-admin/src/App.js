f2139b67b21988c9d301235bc1d598a1e97f1762
Pham Anh <ghuy837@gmail.com>
2023-06-16 23:03:10 +0700

    setup admin environment

--- fe-admin/src/App.js
+++ fe-admin/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/dist";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import PrivateRouter from "./PrivateRouter";
+import { listOrders } from "./redux/Actions/OrderActions";
+import { lisProducts } from "./redux/Actions/ProductActions";
+import "./responsive.css";
+import AddProduct from "./screens/AddProduct";
+import CategoryEditScreen from "./screens/CategoriesEditScreen";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import HomeScreen from "./screens/HomeScreen";
+import Login from "./screens/LoginScreen";
+import NotFound from "./screens/NotFound";
+import OrderDetailScreen from "./screens/OrderDetailScreen";
+import OrderScreen from "./screens/OrderScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
+import ProductScreen from "./screens/productScreen";
+import UsersScreen from "./screens/UsersScreen";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(lisProducts());
+      dispatch(listOrders());
+    }
+  }, [dispatch, userInfo]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <PrivateRouter path="/" component={HomeScreen} exact />
+          <PrivateRouter path="/products" component={ProductScreen} />
+          <PrivateRouter path="/category" component={CategoriesScreen} />
+          <PrivateRouter path="/orders" component={OrderScreen} />
+          <PrivateRouter path="/order/:id" component={OrderDetailScreen} />
+          <PrivateRouter path="/addproduct" component={AddProduct} />
+          <PrivateRouter path="/users" component={UsersScreen} />
+          <PrivateRouter
+            path="/product/:id/edit"
+            component={ProductEditScreen}
+          />
+          <PrivateRouter path="/categories/:id" component={CategoryEditScreen} />
+          <Route path="/login" component={Login} />
+          <PrivateRouter path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
