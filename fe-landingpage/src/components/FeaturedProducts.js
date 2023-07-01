09663dcfdbddb02f9796f33afa5b9e3ece8e7eb6
Pham Anh <contact.vananh0505@gmail.com>
2023-07-01 08:43:35 +0700

    Lấy dữ liệu sản phẩm và hiển thị lên giao diện chi tiết sản phẩm

--- fe-landingpage/src/components/FeaturedProducts.js
+++ fe-landingpage/src/components/FeaturedProducts.js
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container, Col, Row, Card } from 'react-bootstrap'
+// import products from '../products.js'
+import { Link } from 'react-router-dom'
+import '../scss/FeaturedProducts.scss'
+import { listProduct } from '../redux/actions/productActions.js'
 const FeaturedProducts = () => {
+
+    const dispatch = useDispatch();
+
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, products } = productList;
+
+    useEffect(() => {
+        dispatch(listProduct());
+        console.log('list-product', products);
+    }, [dispatch])
+
+
+
     return (
-        <div>FeaturedProducts</div>
+        <Container style={{ marginTop: '5rem' }}>
+            <h3 class="sec-title">SẢN PHẨM NỔI BẬT CỦA LEVENTS®</h3>
+            <Row md={4} xs="auto">
+                {products.slice(0, 8).map((product) => (
+                    <Col>
+                        <Card style={{ border: 'none' }}>
+                            <Link to={`/product/${product._id}`}>
+                                <Card.Img src={product.MainImage} />
+                            </Link>
+                            <Card.Body>
+                                <Card.Title style={{ fontSize: '16px', color: '#7f7f7f' }} >{product.MainName}</Card.Title>
+                                <Card.Text>{product.price},000 vnđ</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
 
